91a3fe005cdceecbd9767089c93316d54a22bb85
xuyang <xuyang@51nbapi.com>
2018-06-28 14:58:01 +0800

    firstgitOrder

---
+++ src/main/java/com/imooc/constant/CookieConstant.java
@@ -0,0 +1,13 @@
+package com.imooc.constant;
+
+/**
+ * cookie常量
+ * Created by 廖师兄
+ * 2017-07-30 16:35
+ */
+public interface CookieConstant {
+
+    String TOKEN = "token333";
+
+    Integer EXPIRE = 7200;
+}
