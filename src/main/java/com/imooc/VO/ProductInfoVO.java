a9ab145565713df6cac7150e540b326417fdb96c
zhudegang <zhudegang@51nbapi.com>
2018-07-05 16:57:56 +0800

    zdg

--- src/main/java/com/imooc/VO/ProductInfoVO.java
+++ src/main/java/com/imooc/VO/ProductInfoVO.java
@@ -13,7 +13,7 @@ import java.math.BigDecimal;
 @Data
 public class ProductInfoVO {
 
-    @JsonProperty("idsell")
+    @JsonProperty("idzdg")
     private String productId;
 
     @JsonProperty("name")
