69b601354f3b208d73d9f9a4eb27ce03182441e4
xuyang <xuyang@51nbapi.com>
2018-07-05 16:05:58 +0800

    1111

--- src/main/java/com/imooc/VO/ProductInfoVO.java
+++ src/main/java/com/imooc/VO/ProductInfoVO.java
@@ -13,7 +13,7 @@ import java.math.BigDecimal;
 @Data
 public class ProductInfoVO {
 
-    @JsonProperty("idfff")
+    @JsonProperty("idhhh")
     private String productId;
 
     @JsonProperty("name")
